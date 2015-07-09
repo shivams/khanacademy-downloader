86ab6e766e6483c8e348a5565eb6feed592e148d
shivams <shivam.src@gmail.com>
2015-07-10 04:15:52 +0530

    First commit

---
+++ khanacademyDownloader.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#curl "$1" | hxnormalize -xe | hxselect a.tutorial-router-link | grep -oP 'href="\K.+?(?=")' | grep "/v/"
+
+curl "$1" | hxnormalize -xe | hxselect div.contents-box li.progress-item a | grep -oP 'href="\K.+?(?=")' | grep "/v/" | while read -r line;
+do
+	link="https://www.khanacademy.org""$line"
+	curl "$link" | grep link | grep youtube | grep video | grep -oP 'href="\K.+?(?=")' | xargs youtube-dl 
+done
+
